5a940b9a8ce1739cbe1f08a7c8e5f7f7d12c66ee
akihiko47 <akihiko4701a@gmail.com>
2025-01-13 03:14:55 +0300

    Dependencies way

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,12 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb/stb_image_write.h"
 
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/hash.hpp>
+
 #include <iostream>
 
 
