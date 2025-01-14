7fc16cedd064bf3bae9cfbfdd12e5c7ac9f1e051
akihiko47 <akihiko4701a@gmail.com>
2025-01-15 01:40:52 +0300

    Smoothness

--- src/main.cpp
+++ src/main.cpp
@@ -12,14 +12,14 @@
 
 
 int main() {
-    art::Image image(1920, 1080);
+    art::Image image(512, 288);
     art::Scene scene;
-    art::Camera camera(glm::vec3(0), 500, 15);
+    art::Camera camera(glm::vec3(0), 300, 15);
 
     art::Lambertian *matGround = new art::Lambertian(glm::vec3(0.4, 0.4, 0.4));
     art::Lambertian *matCenter = new art::Lambertian(glm::vec3(0.8, 0.0, 0.0));
-    art::Metal *matLeft  = new art::Metal(glm::vec3(0.8, 0.8, 0.8));
-    art::Metal *matRight = new art::Metal(glm::vec3(0.8, 0.6, 0.2));
+    art::Metal *matLeft  = new art::Metal(glm::vec3(0.8, 0.8, 0.8), 0.1);
+    art::Metal *matRight = new art::Metal(glm::vec3(0.8, 0.6, 0.2), 0.9);
 
     scene.AddObject(new art::Sphere(glm::vec3( 0.0,    0.0, -1.0),   0.5, matCenter));
     scene.AddObject(new art::Sphere(glm::vec3( 0.0, -100.5, -1.0), 100.0, matGround));
--- src/material.hpp
+++ src/material.hpp
@@ -38,16 +38,18 @@ namespace art {
 
 	class Metal : public Material {
 	public:
-		Metal(const glm::vec3 &albedo) : m_albedo(albedo) {}
+		Metal(const glm::vec3 &albedo = glm::vec3(1), const float smoothness = 0.5) : m_albedo(albedo), m_smoothness(smoothness < 1 ? smoothness : 1) {}
 
 		bool scatter(const Ray &rayIn, const HitInfo &hitInfo, glm::vec3 &attenuation, Ray &rayOut) const override {
 			glm::vec3 reflectDir = glm::reflect(rayIn.GetDirection(), hitInfo.N);
+			reflectDir = glm::normalize(reflectDir) + ((1 - m_smoothness) * RandomVec());
 			rayOut = Ray(hitInfo.p, reflectDir);
 			attenuation = m_albedo;
-			return true;
+			return (glm::dot(rayOut.GetDirection(), hitInfo.N) > 0);  // check if we are not reflecting inside object
 		}
 
 	private:
 		glm::vec3 m_albedo;
+		float m_smoothness;
 	};
 }
