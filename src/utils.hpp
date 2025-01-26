2bd691bf17df8eeca4bfe123db2bf1896b5d596c
akihiko47 <akihiko4701a@gmail.com>
2025-01-27 00:10:39 +0300

    Performance random numbers bug fix

--- src/utils.hpp
+++ src/utils.hpp
@@ -7,9 +7,7 @@ namespace art {
 
 	// Generate random number in range [0, 1)
 	inline float Random() {
-		static std::uniform_real_distribution<double> distribution(0.0, 1.0);
-		static std::mt19937 generator;
-		return distribution(generator);
+		return std::rand() / (RAND_MAX + 1.0);
 	}
 
 	inline float Random(float min, float max) {
